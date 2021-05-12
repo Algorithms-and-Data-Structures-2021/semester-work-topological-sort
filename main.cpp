d544d20bc2e1331527d65c46e40c9fbb6baa3eb0
ilyausanovv <ilya.ysanov.02@list.ru>
2021-05-12 16:44:03 +0300

    Main implementation with sorting output

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-  for (int index = 0; index < argc; index++) {
-    cout << index << ": " << argv[index] << endl;
-  }
+// Программа для тестирования
+int main()
+{
+  // Создаем граф
+  Graph g(6);
+  g.addEdge(5, 2);
+  g.addEdge(5, 0);
+  g.addEdge(4, 0);
+  g.addEdge(4, 1);
+  g.addEdge(2, 3);
+  g.addEdge(3, 1);
+
+  cout << "Following is a Topological Sort of the given graph: \n";
+  g.topologicalSort();
+
   return 0;
 }
